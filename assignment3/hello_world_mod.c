1226a1bdce23df1ffae2c036dd89ae512bd57a91
John Vincent Angeles <jayveeangeles@gmail.com>
2017-03-28 23:05:54 +0800

    adding hello world kernel init example...

---
+++ assignment3/hello_world_mod.c
@@ -0,0 +1,20 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h> //for module_param
+MODULE_LICENSE("GPL");
+
+static char *who = "world";
+module_param(who, charp, 0644);
+MODULE_PARM_DESC(who, "Tell me your name.");
+
+static int hello_init(void) {
+    printk(KERN_ALERT "Hello, %s!\n", who);
+    return 0;
+}
+static void hello_exit(void) {   
+    printk(KERN_ALERT "Goodbye, cruel world\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
