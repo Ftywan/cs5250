113c880493f15c1969e6028549eed39ffeb13360
John Vincent Angeles <jayveeangeles@gmail.com>
2017-04-14 19:56:33 +0800

    fixing helper script to ensure run as root..

--- assignment4/helper_script_one.sh
+++ assignment4/helper_script_one.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
+if [[ "$EUID" -ne 0 ]]; then
+    echo "script must be run as root"
+    exit 1
+fi
+
+function check_return()
+{
+    local str="$@"
+    if [[ "$?" -ne 0 ]]; then
+        echo "operation $str failed.. quitting now.."
+        exit 2
+    else 
+        echo $str
+        printf "\n"
+    fi
+}
+
 rmmod char_driver
-echo "removed char module"
+check_return "removed char module"
 rm -rf /dev/char_driver
-echo "removed char driver"
+check_return "removed char driver"
 mknod /dev/char_driver c 61 0
-echo "created a new char driver"
+check_return "created a new char driver"
 make clean
-echo "finished cleaning files"
+check_return "finished cleaning files"
 make all
-echo "finished compiling char_driver"
+check_return "finished compiling char_driver"
 insmod char_driver.ko
-echo "inserted char driver"
+check_return "inserted char driver"
 gcc char_test.c -o char_test -g -Wall
-echo "compiled char_test.c"
+check_return "compiled char_test.c"
 gcc test.c -o test -g -Wall
-echo "compiled test.c"
+check_return "compiled test.c"
 gcc test2.c -o test2 -g -Wall
-echo "compiled test2.c"
+check_return "compiled test2.c"
 dmesg -C
-echo "cleared dmesg contents"
+check_return "cleared dmesg contents"
